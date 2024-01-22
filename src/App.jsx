24d11867d1fffbd35e4f269cd73f85aabbef128b
Kevin Antonio Borge Gutierrez <108099077+PixelGenetics@users.noreply.github.com>
2024-01-21 19:03:41 -0600

    Making changes on return

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import phrases from './utils/phrases.json'
+import RandomPhrase from './components/RandomPhrase';
+import randomIndex from './Services/randomIndex';
+import ButtonPhrase from './components/ButtonPhrase';
+import bgArray from './utils/bgArray.json'
 
 function App() {
-  const [frase, setFrase] = useState("")
+  const sentence = randomIndex(phrases);
+  const bgIndex = randomIndex(bgArray);
+  const [quote, setQuote] = useState(sentence);
+  const [bgApp, setBgApp] = useState(bgIndex)
 
-  function randomInt(num) {
-    return Math.floor(Math.random() * num);
+  const bgStyle = {
+    backgroundImage: `url('../src/assets/fondo${bgApp}.png')`
   }
 
-  useEffect(() => {
-    obtenerFraseAleatoria();
-  },[]);
-
-  function obtenerFraseAleatoria() {
-    const jsonSize = phrases.length;
-    const jsonSize2 = randomInt(jsonSize);
-    const fraseAleatoria = phrases[jsonSize2];
-
-    setFrase(fraseAleatoria);
-    console.log('frase',frase)
-  }
   return (
-    <div className='app'>
+    <div className='app' style={bgStyle}>
       <div className='container'>
           <h1>GALLETAS DE LA FORTUNA</h1>
-          <button onClick={obtenerFraseAleatoria}>Ver otro</button>
-          <div className='phraseAuthor'>
-            <p >{frase.phrase}</p>
-            <p >Fuente: {frase.author}</p>
+          <ButtonPhrase setQuote={setQuote} setBgApp={setBgApp}/>
+            <RandomPhrase quote={quote} />
           </div>
         </div>
-    </div>
   )
 }
 
