d905d68c17308369f5ecd4cf9c657fc502cfa673
Kevin Antonio Borge Gutierrez <108099077+PixelGenetics@users.noreply.github.com>
2024-01-21 19:42:30 -0600

    Adding logic to bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import RandomPhrase from './components/RandomPhrase';
 import randomIndex from './Services/randomIndex';
 import ButtonPhrase from './components/ButtonPhrase';
 import bgArray from './utils/bgArray.json'
+import fondo1 from '../src/assets/fondo1.png'
+import fondo2 from '../src/assets/fondo2.png'
+import fondo3 from '../src/assets/fondo3.png'
+import fondo4 from '../src/assets/fondo4.png'
 
 function App() {
   const sentence = randomIndex(phrases);
@@ -12,12 +16,13 @@ function App() {
   const [quote, setQuote] = useState(sentence);
   const [bgApp, setBgApp] = useState(bgIndex)
 
-  const bgStyle = {
-    backgroundImage: `url('../src/assets/fondo${bgApp}.png')`
-  }
+  const bgStyle = [fondo1,fondo2,fondo3,fondo4]
 
+  const x = randomIndex(bgStyle);
+    
   return (
-    <div className='app' style={bgStyle}>
+    <div className='app'>
+      <img src={x} alt="" className='fondoImage'/>
       <div className='container'>
           <h1>GALLETAS DE LA FORTUNA</h1>
           <ButtonPhrase setQuote={setQuote} setBgApp={setBgApp}/>
